419c9ca776c14ae41a124c6c66135e28d793d980
Dikla Tzemah Weyl <Dikla.Weyl@fundtech.isr>
2017-08-07 15:23:45 +0300

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PageHeader} from '../components/pageHeader/pageHeader';
 
 @NgModule({
   declarations: [
     MyApp,
+    PageHeader,
     HomePage
   ],
   imports: [
@@ -19,6 +21,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    PageHeader,
     HomePage
   ],
   providers: [
---
+++ src/components/pageHeader/pageHeader.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the PageHeaderComponent component.
+ *
+ * See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
+ * for more info on Angular Components.
+ */
+@Component({
+  selector: 'page-header',
+  templateUrl: 'pageHeader.html'
+})
+export class PageHeader {
+
+
+
+  constructor() {
+   
+  }
+
+}
