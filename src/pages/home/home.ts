01729c30adc8a3ecfc5f2ae392b17878bac5398a
Dikla Tzemah Weyl <Dikla.Weyl@fundtech.isr>
2017-08-09 15:47:50 +0300

    sign up page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { UserBasic } from './userBasic';
+
 import {SignUpPage} from '../signUpPage/signUpPage'
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
@@ -29,31 +29,18 @@ import 'rxjs/add/operator/map';
   ]
 })
 export class HomePage {
-  myUser: UserBasic = <UserBasic>{};
-  diklaString: string;
+
 
   constructor(protected nav: NavController, public http: Http) { }
 
-  postPerson() {
-    let headers = new Headers;
-    headers.append('Content-Type', 'application/json');
+  
 
-
-    let body = { name: this.myUser.name, age: this.myUser.age, familyName: this.myUser.nameFamily };
-
-    this.http.post('http://localhost:8080/user', JSON.stringify(body), { headers: headers })
-      .map(res => res.json())
-      .subscribe(data => { console.log(data); });
-
-
-  }
-
-  getDikla() {
-    this.http.get('http://localhost:8080/userget')
-      .map((res: Response) => { return res/*.json() */ })
-      .subscribe(data => { debugger; this.diklaString = data['_body'] },
-      err => { console.log("Oops!"); });
-  }
+  // getDikla() {
+  //   this.http.get('http://localhost:8080/userget')
+  //     .map((res: Response) => { return res/*.json() */ })
+  //     .subscribe(data => { debugger; this.diklaString = data['_body'] },
+  //     err => { console.log("Oops!"); });
+  // }
 
 
   goToSignUp(){
--- src/pages/signUpPage/signUpPage.ts
+++ src/pages/signUpPage/signUpPage.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {UserBasic} from '../../interfaces/UserBasic'
 
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
@@ -11,7 +12,23 @@ import 'rxjs/add/operator/map';
 })
 
 export class SignUpPage {
+myUser: UserBasic = <UserBasic>{};
+  diklaString: string;
   
   constructor(protected nav: NavController, public http: Http) { }
+
+  submitUser() {
+    let headers = new Headers;
+    headers.append('Content-Type', 'application/json');
+
+
+    let body = { name: this.myUser.name, age: this.myUser.age, familyName: this.myUser.nameFamily };
+
+    this.http.post('http://localhost:8080/user', JSON.stringify(body), { headers: headers })
+      .map(res => res.json())
+      .subscribe(data => { console.log(data); });
+
+
+  }
 
 }
